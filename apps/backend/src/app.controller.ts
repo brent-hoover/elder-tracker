a9e3899ec55ae0933eaf8ba69bc60252f4e6c969
Brent Hoover <brent@thebuddhalodge.com>
2025-07-18 20:58:22 -0700

    fix: fix healthcheck endpoint

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('health')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +10,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('health')
+  getHealth(): { status: string; timestamp: string } {
+    return {
+      status: 'ok',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
